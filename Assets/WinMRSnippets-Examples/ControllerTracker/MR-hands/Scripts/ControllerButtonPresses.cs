486be60ea842fc4657db12a78f17eb04dba88bd7
Jaime Rodriguez <jaimer@live.com>
2018-02-28 00:51:39 -0800

    Mr hands

---
+++ Assets/WinMRSnippets-Examples/ControllerTracker/MR-hands/Scripts/ControllerButtonPresses.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA.Input;
+
+public class ControllerButtonPresses : MonoBehaviour {
+
+    public bool isRightController;
+    private bool pressTrigger = false;
+    private bool pressGrip = false;
+    private bool pressThumbstickUp = false;
+    private bool pressTouchpadR = false;
+    private bool pressTouchpadL = false;
+
+
+    void Start () {
+        InteractionManager.InteractionSourcePressed += InteractionManager_InteractionSourcePressed;
+        InteractionManager.InteractionSourceReleased += InteractionManager_InteractionSourceReleased;
+        InteractionManager.InteractionSourceUpdated += InteractionManager_InteractionSourceUpdated;
+	}
+
+    private void InteractionManager_InteractionSourceUpdated(InteractionSourceUpdatedEventArgs obj)
+    {
+        if ((obj.state.source.handedness == InteractionSourceHandedness.Right && isRightController) || (obj.state.source.handedness == InteractionSourceHandedness.Left && !isRightController))
+        {
+            if (obj.state.thumbstickPosition.y > 0.5f && !pressThumbstickUp)
+            {
+                Debug.Log("Thumbstick Up");
+                gameObject.GetComponent<Animator>().SetBool("TriggerPress", false);
+                gameObject.GetComponent<Animator>().SetBool("GripPress", false);
+                gameObject.GetComponent<Animator>().SetBool("ThumbstickUp", true);
+                pressThumbstickUp = true;
+            }
+            else if (obj.state.thumbstickPosition.y < 0.5f)
+            {
+                gameObject.GetComponent<Animator>().SetBool("ThumbstickUp", false);
+                pressThumbstickUp = false;
+            }
+
+        }
+
+        if (!obj.state.touchpadPressed)
+        {
+            gameObject.GetComponent<Animator>().SetBool("EasterEgg", false);
+            pressTouchpadR = false; pressTouchpadL = false;
+        }
+        else if (obj.state.touchpadPressed && obj.state.source.handedness == InteractionSourceHandedness.Right && !pressTouchpadR)
+        {
+            pressTouchpadR = true;
+        }
+        else if (obj.state.touchpadPressed && obj.state.source.handedness == InteractionSourceHandedness.Left && !pressTouchpadL)
+        {
+            pressTouchpadL = true;
+        }
+
+        if (pressTouchpadR && pressTouchpadL)
+        {
+            Debug.Log("Both Touchpads Pressed");
+            gameObject.GetComponent<Animator>().SetBool("EasterEgg", true);
+        }
+    }
+
+    private void InteractionManager_InteractionSourceReleased(InteractionSourceReleasedEventArgs obj)
+    {
+        if ((obj.state.source.handedness == InteractionSourceHandedness.Right && isRightController) || (obj.state.source.handedness == InteractionSourceHandedness.Left && !isRightController))
+        {
+            if (pressTrigger)
+            {
+                gameObject.GetComponent<Animator>().SetBool("TriggerPress", false);
+                pressTrigger = false;
+            }
+            else if (pressGrip)
+            {
+                gameObject.GetComponent<Animator>().SetBool("GripPress", false);
+                pressGrip = false;
+            }
+        }
+    }
+
+    private void InteractionManager_InteractionSourcePressed(InteractionSourcePressedEventArgs obj)
+    {
+        if ((obj.state.source.handedness == InteractionSourceHandedness.Right && isRightController) || (obj.state.source.handedness == InteractionSourceHandedness.Left && !isRightController))
+        {
+            gameObject.GetComponent<Animator>().SetBool("GripPress", false);
+            gameObject.GetComponent<Animator>().SetBool("TriggerPress", false);
+            gameObject.GetComponent<Animator>().SetBool("ThumbstickUp", false);
+            gameObject.GetComponent<Animator>().SetBool("EasterEgg", false);
+            if (obj.state.selectPressedAmount > 0.1f && !pressTrigger)
+            {
+                Debug.Log("Select Pressed");
+                gameObject.GetComponent<Animator>().SetBool("TriggerPress", true);
+                pressTrigger = true;
+            }
+            if (obj.state.grasped && !pressGrip)
+            {
+                Debug.Log("Grasp Pressed");
+                gameObject.GetComponent<Animator>().SetBool("GripPress", true);
+                pressGrip = true;
+            }
+        }
+    }
+
+
+}
---
+++ Assets/WinMRSnippets-Examples/ControllerTracker/Scripts/HandsAnimator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using WinMRSnippets; 
+
+[RequireComponent(typeof(Animator))]
+public class HandsAnimator : MonoBehaviour {
+
+    [SerializeField]
+    TrackedController trackedController;
+     
+    private Animator animator; 
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        Debug.Assert(animator != null); 
+
+        if ( trackedController == null)
+        {
+            Debug.LogError("Tracked controller is required");
+            Destroy(this); 
+        }
+
+        trackedController.SelectPressed += OnSelectPressed;
+        trackedController.GraspPressed += OnGraspPressed;
+        trackedController.TouchpadPressed += OnTouchpadPressed;
+        trackedController.SelectReleased += OnSelectReleased;
+        trackedController.GraspReleased += OnGraspReleased;
+        trackedController.TouchpadReleased += OnTouchpadReleased;
+
+
+    }	 
+
+
+    void OnSelectPressed(object sender, MotionControllerStateChangedEventArgs args)
+    {
+        animator.SetBool("TriggerPress", true);
+    }
+
+    void OnSelectReleased(object sender, MotionControllerStateChangedEventArgs args)
+    {
+        animator.SetBool("TriggerPress", false );
+    }
+
+
+    void OnGraspPressed(object sender, MotionControllerStateChangedEventArgs args)
+    {
+        animator.SetBool("GripPress", true);
+    }
+
+    void OnGraspReleased(object sender, MotionControllerStateChangedEventArgs args)
+    {
+        animator.SetBool("GripPress", false );
+    }
+
+    void OnTouchpadPressed (object sender, MotionControllerStateChangedEventArgs args)
+    {
+        animator.SetBool("EasterEgg", true);
+    }
+
+    void OnTouchpadReleased(object sender, MotionControllerStateChangedEventArgs args)
+    {
+        animator.SetBool("EasterEgg", false);
+    }
+
+}
+
