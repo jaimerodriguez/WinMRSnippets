f03521330802aaab6d8d54b79d69218f58d8fdd4
Eric Ahlstrom <eric@ericahlstrom.com>
2018-02-27 16:28:57 -0800

    initial throwing field

---
+++ Assets/WinMRSnippets-Examples/ThrowingField/Scripts/AccuracyTracker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class AccuracyTracker : MonoBehaviour {
+
+    MeshRenderer myRenderer = null;
+    // Use this for initialization
+    void Start()
+    {
+        myRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var readings = UnityEngine.XR.WSA.Input.InteractionManager.GetCurrentReading();
+
+        if (readings.Length > 0)
+        {
+            foreach (var reading in readings)
+            {
+                Color c = GetColor(reading.properties.sourceLossRisk);
+                myRenderer.material.color = c;
+            }
+        }
+    }
+
+    double readings = 0;
+    Color GetColor(InteractionSourcePositionAccuracy f)
+    {
+        switch (f)
+        {
+            case InteractionSourcePositionAccuracy.High:
+                return Color.green;
+            case InteractionSourcePositionAccuracy.Approximate:
+                return Color.yellow;
+            default:
+                return Color.red;
+        }
+    }
+
+    Color GetColor(double f)
+    {
+        readings += f;
+
+        if (f > 1.2)
+            return Color.red;
+        else if (f > .9)
+            return Color.yellow;
+        else
+            return Color.green;
+    }
+}
---
+++ Assets/WinMRSnippets-Examples/ThrowingField/Scripts/ColliderDetect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDetect : MonoBehaviour {
+
+    public ParticleSystem ps;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("It went through!!!");
+        ps.Play();
+    }
+
+}
